8be899d3742de95c4a0837c7a6693a2fc9316272
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-02-19 10:00:30 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const Base_URL="https://raw.githubusercontent.com/WoXy-Sensei/currency-api/main/api"
+
 const dropdown = document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button");
+
+const currFrom=document.querySelector(".from select");
+const currTo=document.querySelector(".to select");
+
+const msg=document.querySelector(".msg");
 
 for (let select of dropdown) {
     for (let currCode in countryList) {
@@ -22,5 +30,34 @@ for (let select of dropdown) {
 
 
 const updateFlag=(element)=>{
-    console.log(element);
+    let currcode=element.value
+    let countryCode=countryList[currcode];
+    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`
+    let img=element.parentElement.querySelector("img");
+    img.src=newSrc;
+
 }
+
+btn.addEventListener("click",async(evt)=>{
+    evt.preventDefault();
+    let amt=document.querySelector(".amount input");
+    let amtVal=amt.value;
+    if(amtVal==="" || amtVal<1)
+    {
+        amtVal=1;
+        amt.value="1";
+    }
+    console.log(currFrom.value);
+    console.log(currTo.value)
+    const url=`${Base_URL}/${currTo.value}_${currFrom.value}.json`;
+    
+    let response=await fetch(url);
+    let data=await response.json();
+    let rate=data.rate*amtVal;
+    console.log(rate)
+    const final_amount=`${amtVal} ${currFrom.value} = ${rate} ${currTo.value}`;
+    msg.innerText=final_amount;
+});
+
+
+
